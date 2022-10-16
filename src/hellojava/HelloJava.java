aefa365940eae806f13ed0ac5fcbf964998cb18a
Erfan <erfanmelon@yahoo.com>
2022-10-16 14:23:22 +0330

    Initial commit

---
+++ src/hellojava/HelloJava.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hellojava;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Erfan
+ */
+public class HelloJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        System.out.print("Please Enter "
+                + "\n + For Sum "
+                + "\n - For Subtract "
+                + "\n * For Product "
+                + "\n / For Division "
+                + "\n:");
+        Scanner inputs = new Scanner(System.in);
+        String input = inputs.nextLine();
+        System.out.print("Please Enter First Number : ");
+        long num1 = inputs.nextLong();
+        System.out.print("Please Enter Second Number : ");
+        long num2 = inputs.nextLong();
+
+        switch (input) {
+            case "+":
+                System.out.println("Sum is :" + (num1 + num2));
+                break;
+            case "-":
+                System.out.println("Subtract is :" + (num1 - num2));
+                break;
+            case "*":
+                System.out.println("Product is :" + (num1 * num2));
+                break;
+            case "/":
+                System.out.println("Division is :" + (num1 / num2));
+                break;
+            default:
+                System.err.println("Error , Choose Valid Operand !");
+                main(new String[1]);
+                break;
+        }
+    }
+
+}
